1ce97c04fea75b53b5c179647bf7857de4effc72
ProTheDev <tebogoalbanlefatola@gmail.com>
2022-06-12 00:50:30 -0700

    Added first batch of program..

---
+++ src/main/java/GreetServer.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GreetServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        // when creating a server - it will have to be given a port number from which it must run at
+        // once the port number number has been provided the server object is then created and starts listening for client connection
+        serverSocket = new ServerSocket(port);
+        // this is the point at which client connection is bindend to that of the server
+        clientSocket = serverSocket.accept();
+
+        // messeges sent to the client are sent using PrintWriter
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        // and messeges recieved from the client are read using BufferedReader
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        // the readline() method reads input from the client on a line
+        String greeting = in.readLine();
+        //at this point the server should have a string value of the message sent by the client
+        // further processing can happen from this point
+        if ("hello server".equals(greeting)) {
+            out.println("hello client");
+        } else {
+            out.println("unrecognised greeting");
+        }
+
+    }
+
+    public void stop() throws IOException {
+        // usually when working with servers, alot of streams are open
+        // ideally we would not have much of an issue with them
+        // but because they still continue to use up system resources
+        // we have to switch them off once again
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        // a quite weired thing about servers is that when you start them from their main, you also need to have created an instance of them as follows
+        GreetServer server = new GreetServer();
+        server.start(5000);
+
+    }
+}
---
+++ src/test/java/TestClientServerConnection.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class TestClientServerConnection {
+    // to run tests on a server you fisrt need to turn it on
+    @Test
+    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
+
+        GreetClient client = new GreetClient();
+        client.startConnection("localhost", 5000);
+        String response = client.sendMessage("hello server");
+        assertEquals("hello client", response);
+
+    }
+}
